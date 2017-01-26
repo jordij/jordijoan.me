6d05e50c1f4426d3c1f024136fa009679a956d3a
Jordi <jordi.joan@gmail.com>
2017-01-26 20:51:16 +1300

    Packages updates and code style css updates. (#1)

--- core/blocks.py
+++ core/blocks.py
@@ -1,4 +1,9 @@
+from pygments import highlight
+from pygments.formatters import get_formatter_by_name
+from pygments.lexers import get_lexer_by_name
+
 from django.conf import settings
+from django.utils.safestring import mark_safe
 
 from wagtail.wagtailcore import blocks
 from wagtail.wagtailimages.blocks import ImageChooserBlock
@@ -30,10 +35,34 @@ class CodeBlock(blocks.StructBlock):
         ('html', 'HTML'),
         ('css', 'CSS'),
         ('scss', 'SCSS'),
+        ('json', 'JSON'),
+    )
+
+    STYLE_CHOICES = (
+        ('syntax', 'default'),
+        ('emacs', 'emacs'),
+        ('monokai', 'monokai'),
+        ('vim', 'vim'),
+        ('xcode', 'xcode'),
     )
 
     language = blocks.ChoiceBlock(choices=LANGUAGE_CHOICES)
+    style = blocks.ChoiceBlock(choices=STYLE_CHOICES, default='syntax')
     code = blocks.TextBlock()
+
+    def render(self, value):
+        src = value['code'].strip('\n')
+        lang = value['language']
+        lexer = get_lexer_by_name(lang)
+        css_classes = ['code', value['style']]
+
+        formatter = get_formatter_by_name(
+            'html',
+            linenos=None,
+            cssclass=' '.join(css_classes),
+            noclasses=False,
+        )
+        return mark_safe(highlight(src, lexer, formatter))
 
     class Meta:
         icon = 'code'
